5a982249eb3a620dafefe01f32f227bcdd0a44ed
ElPardo16 <andresito105@gmail.com>
2023-03-01 16:50:14 -0500

    merge

--- src/pages/reset/[token].js
+++ src/pages/reset/[token].js
@@ -1,17 +1,10 @@
-import { useRouter } from 'next/router'
 import React from 'react'
 import Recovery from '../../components/Recovery'
 
 export default function prueba() {
-
-    const router = useRouter()
-    const { token } = router.query
-
   return (
-    <div><Recovery/>
-    <p>
-        {token}
-    </p>
+    <div>
+      <Recovery/>
     </div>
   )
 }
